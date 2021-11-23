80b182080a7dde9b1326972e86062e9e79239581
Masatoon0326 <62167501+Masatoon0326@users.noreply.github.com>
2021-11-23 20:52:51 +0900

    Update webcam.js

--- webcam.js
+++ webcam.js
@@ -1,6 +1,4 @@
 const videoElement = document.querySelector('video');
-const audioInputSelect = document.querySelector('select#audioSource');
-const audioOutputSelect = document.querySelector('select#audioOutput');
 const videoSelect = document.querySelector('select#videoSource');
 const selectors = [videoSelect];
 
